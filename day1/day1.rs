8f58b671dbd9d4aabb18d6cd4cd38085d14870b0
Zack Gottesman <zachary.s.gottesman.22@dartmouth.edu>
2022-12-07 12:47:57 -0800

    updating day 1 code to include part 2

--- day1/day1.rs
+++ day1/day1.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 
 fn main() {
     // File hosts must exist in current path before this produces output
-    let max = handle_input("input1.txt");
+    let max = handle_input_part_2("input1.txt");
     println!("{max}")
 }
 
@@ -18,7 +18,7 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn handle_input(filename: &str) -> i32 {
+fn handle_input_part_1(filename: &str) -> i32 {
     if let Ok(lines) = read_lines(filename) {
         let mut curr_group_sum = 0;
         let mut max_group_sum = 0;
@@ -43,3 +43,56 @@ fn handle_input(filename: &str) -> i32 {
         panic!();
     }
 }
+
+fn handle_input_part_2(filename: &str) -> i32 {
+    if let Ok(lines) = read_lines(filename) {
+        // keep track of top 3
+        // something for sure goes in top 3 if it's larger than smallest of current top 3
+        let mut top3: [i32; 3] = [-1, -1, -1];
+        let mut curr_group_sum: i32 = 0;
+
+        for line in lines {
+            if let Ok(ip) = line {
+                if ip == "" {
+                    insert_into_top3(&mut top3, curr_group_sum);
+                    curr_group_sum = 0;
+                } else {
+                    let line_as_int: i32 = ip.parse().unwrap();
+                    curr_group_sum += line_as_int;
+                }
+            }
+        }
+
+        return top3[0] + top3[1] + top3[2];
+    } else {
+        panic!();
+    }
+}
+
+fn insert_into_top3(t3_p: &mut [i32; 3], val: i32) {
+    for i in 0..3 {
+        if t3_p[i] == -1 {
+            t3_p[i] = val;
+
+            return;
+        }
+    }
+
+    let mut i: usize = 0;
+    while i < 3 && val > t3_p[i] {
+        i += 1;
+    }
+
+    if i > 0 {
+        while i > 0 {
+            temp = t3_p[i - 1];
+            t3_p[i - 1] = val;
+            t3_p[i - 2] = temp;
+            i -= 1;
+        }
+    }
+}
+
+// desired traits for data structure:
+//  - easy to check minimum element in top 3
+//  - easy to place new element in top 3 if it belongs, replacing minimum
