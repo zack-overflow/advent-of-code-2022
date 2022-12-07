f6ed291830f602de41d3ec3cad87aaf58199cc40
Zack Gottesman <zachary.s.gottesman.22@dartmouth.edu>
2022-12-07 13:19:45 -0800

    updates to top 3 structure

--- day1/day1.rs
+++ day1/day1.rs
@@ -48,13 +48,18 @@ fn handle_input_part_2(filename: &str) -> i32 {
     if let Ok(lines) = read_lines(filename) {
         // keep track of top 3
         // something for sure goes in top 3 if it's larger than smallest of current top 3
-        let mut top3: [i32; 3] = [-1, -1, -1];
+        let mut top3: top3_t = {
+            all_set: false,
+            t3_items: HashSet::new(),
+            smallest: f32::INFINITY,
+        };
+
         let mut curr_group_sum: i32 = 0;
 
         for line in lines {
             if let Ok(ip) = line {
                 if ip == "" {
-                    insert_into_top3(&mut top3, curr_group_sum);
+                    insert_into_top3(top3, curr_group_sum);
                     curr_group_sum = 0;
                 } else {
                     let line_as_int: i32 = ip.parse().unwrap();
@@ -63,32 +68,26 @@ fn handle_input_part_2(filename: &str) -> i32 {
             }
         }
 
-        return top3[0] + top3[1] + top3[2];
+        // return sum of top 3
+        let mut sum = 0;
+        for item in top3.t3_items {
+            sum += item;
+        }
+
+        return sum;
     } else {
         panic!();
     }
 }
 
-fn insert_into_top3(t3_p: &mut [i32; 3], val: i32) {
-    for i in 0..3 {
-        if t3_p[i] == -1 {
-            t3_p[i] = val;
-
-            return;
-        }
-    }
-
-    let mut i: usize = 0;
-    while i < 3 && val > t3_p[i] {
-        i += 1;
-    }
-
-    if i > 0 {
-        while i > 0 {
-            temp = t3_p[i - 1];
-            t3_p[i - 1] = val;
-            t3_p[i - 2] = temp;
-            i -= 1;
+fn insert_into_top3(t3_p: top3_t, val: i32) {
+    if !t3_p.all_set {
+        t3_p.t3_items.insert(val);
+        set_smallest(t3_p);
+    } else {
+        if val > t3_p.smallest {
+            t3_p.t3_items.remove(t3_p.smallest);
+            set_smallest(t3_p);
         }
     }
 }
@@ -96,3 +95,20 @@ fn insert_into_top3(t3_p: &mut [i32; 3], val: i32) {
 // desired traits for data structure:
 //  - easy to check minimum element in top 3
 //  - easy to place new element in top 3 if it belongs, replacing minimum
+struct top3_t {
+    all_set: bool,
+    t3_items: HashSet<i32>,
+    smallest: i32,
+}
+
+fn set_smallest(t3: t3_p) {
+    let mut smallest = f32::INFINITY;
+    
+    for item in t3.t3_items {
+        if item < smallest {
+            smallest = item;
+        }
+    }
+
+    t3.smallest = smallest;
+}
